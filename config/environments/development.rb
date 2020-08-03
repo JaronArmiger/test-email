c96a258495b0231cda55370c6025bdb1fbe5a740
JaronArmiger <jaron.armiger@gmail.com>
2020-08-03 10:58:19 -0500

    add local_env.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_APP_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
